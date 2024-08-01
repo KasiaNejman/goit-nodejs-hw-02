3bf0b2346b3c0664e41290842d6a293b7295bca1
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-08-01 14:25:39 +0200

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connection successful");
+  } catch (error) {
+    console.error("Database connection error:", error.message);
+    process.exit(1);
+  }
+};
