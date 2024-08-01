eac9c33e6e94faf3944482b4e19238d8658629ac
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-08-01 13:34:42 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,31 +1,38 @@
-const { Schema, models, model } = require("mongoose");
+const contacts = require("./contacts.json");
 
-const ContactSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Set name for contact"],
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: [true, "Set email for contact"],
-    },
-    phone: {
-      type: String,
-    },
-    favourite: {
-      type: Boolean,
-      default: false,
-    },
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-  },
-  { versionKey: false, timestamps: true }
-);
+const listContacts = async () => {
+  return contacts;
+};
 
+const getContactById = async (contactId) => {
+  return contacts.find((contact) => contact.id === contactId);
+};
+
+const removeContact = async (contactId) => {
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) return false;
+  contacts.splice(index, 1);
+  return true;
+};
+
+const addContact = async (body) => {
+  contacts.push(body);
+};
+
+const updateContact = async (contactId, body) => {
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) return null;
+  contacts[index] = { ...contacts[index], ...body };
+  return contacts[index];
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
 const Contact = models?.Contact || model("contacts", ContactSchema);
 
 module.exports = { Contact };
