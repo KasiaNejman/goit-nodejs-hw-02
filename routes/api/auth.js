f0f6ea4fd71add3552d3a651655c2c23ad35228f
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-08-01 12:00:20 +0200

    Create auth.js

---
+++ routes/api/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const ctrlAuth = require("../../controllers/authController");
+const authMiddleware = require("../../middlewares/authenticate");
+
+const router = express.Router();
+
+router.post("/register", ctrlAuth.register);
+router.post("/login", ctrlAuth.login);
+router.get("/current", authMiddleware, ctrlAuth.getCurrent);
+router.post("/logout", authMiddleware, ctrlAuth.logout);
+router.get("/verify/:verificationToken", ctrlAuth.verifyEmail);
+router.post("/verify", ctrlAuth.resendVerifyEmail);
+
+module.exports = router;
