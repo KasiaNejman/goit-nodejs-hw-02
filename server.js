af4a6dc81921ee96a6f6c0d400f44e0f2cc02470
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-08-01 12:10:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,47 @@
-const app = require('./app')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const passport = require("passport");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const JwtStrategy = require("./config/jwt.js");
+const PORT = process.env.PORT || 3000;
+const DB_HOST = process.env.DB_HOST;
+const contactsRouter = require("./routes/api/contacts.js");
+const authRouter = require("./routes/api/auth.js");
+const avatarRouter = require("./routes/api/avatars");
+const authMiddleware = require("./middlewares/authenticate.js");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = express();
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(passport.initialize());
+passport.use(JwtStrategy);
+
+app.use("/api/contacts", contactsRouter);
+app.use("/users", authRouter);
+app.use("/users/avatars", authMiddleware, avatarRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+    console.log(`Server is running on port ${PORT}`);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
