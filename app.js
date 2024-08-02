e4e9fdf95bf9abad332c1b41bd048da7867ac91d
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-08-02 11:05:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const contactsRoutes = require("./routes/contactsRoutes");
 
-const contactsRouter = require('./routes/api/contacts')
+connectDB();
 
-const app = express()
+const contactsRouter = require("./routes/api/contacts");
+const usersRouter = require("./routes/api/usersRoutes");
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const app = express();
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use('/api/contacts', contactsRouter)
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+app.use("/api/contacts", contactsRoutes);
+
+app.use("/avatars", express.static("public/avatars"));
+
+module.exports = app;
