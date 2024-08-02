b0a04b190f20b237f29a0c78b86c0897b82504dd
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-08-02 13:09:24 +0200

    Create  user.js

---
+++ models/ user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+
+const userSchema = new mongoose.Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    required: [true, "Verify token is required"],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
